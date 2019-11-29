6a6e5c8c1bfd3ed2105acf7a433e898f6d64736a
GabrielSFelix <gabrielsfelix@gmail.com>
2019-11-28 22:17:59 -0300

    adicionado o quarto echo para ver se causa algum conflito

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 	echo "Hello World alterado! sem ideia que foi alterado no github";
 	echo "echo dois e alteração do arquivo remoto";
 	echo "três";
+	echo "alteração no arquivo remoto mas sem alteração no local";
 
 ?>
